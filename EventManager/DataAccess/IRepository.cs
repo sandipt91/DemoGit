563121d182c98dcbba6d5dc0b20b7a5d1d9d24eb
sandip <sandipt91@gmail.com>
2015-08-22 15:33:39 +0530

    base code

---
+++ EventManager/DataAccess/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        //T Find(int id);
+        //List<T> GetAll(int pageNumber, string pageSize);
+        //bool Update(T entity);
+        //bool Save(T entity);
+        //bool Delete(int id, Int64 modifiedBy);
+    }
+}
